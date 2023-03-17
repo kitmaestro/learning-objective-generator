429b0bee251ee16fe3d053e57a113a603858a43b
Otoniel Reyes <kenliten@otonielreyes.com>
2023-03-17 17:49:43 -0400

    Added help and version commands

--- app/learning-objective-generator.js
+++ app/learning-objective-generator.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+const lib = require('./index');
+const process = require('process');
+const pkg = require('../package.json');
 
-require('@babel/register')({
-  only: [
-    './app'
-  ]
-});
-require('./index');
+if (['-h', '--help', '-?'].includes(process.argv[2].toLowerCase())) {
+    console.log('Hello World!');
+}
+
+if (['-v', '--version'].includes(process.argv[2].toLowerCase())) {
+    console.log(`leobgen v${pkg.version}. By kenliten at KitMaestro.`);
+}
